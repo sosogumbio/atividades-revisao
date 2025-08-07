e53283c29dce181fe1abb3bdc9763064bc308046
sosogumbio <sophiaisabelle102@gmail.com>
2025-08-07 11:39:38 -0300

    Delete node_modules directory

--- node_modules/typescriptnpm/index.ts
+++
@@ -1,9 +0,0 @@
-import * as pluralize from 'pluralize'
-/**
-* @Method: Returns the plural form of any noun.
-* @Param {string}
-* @Return {string}
-*/
-export function getPlural (str: any) : string {
-  return pluralize.plural(str)
-}
